b655e8e88071f7572e6ec80fb0587425a9dcd3db
Tomás Hernández <code@thernandez.me>
2022-09-18 04:44:13 -0300

    feat: 🎸 Exercise 01 - Hello World

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![allow(unconditional_recursion)]
 
+#[path = "./01-hello-world/main.rs"] mod hello_world;
+
 pub fn main() {
   let mut input = String::new();
   std::io::stdin().read_line(&mut input).unwrap();
@@ -7,6 +9,7 @@ pub fn main() {
   print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
 
   match input.trim(){
+    "01" => println!("{}", crate::hello_world::main()),
     _ => {},
   }
 
