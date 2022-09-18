b2c819ec30cebc87446a56c71dcee84fe5193804
Tomás Hernández <code@thernandez.me>
2022-09-18 18:58:14 -0300

    style: 💄 Run rustfmt formatter

--- src/02-lucian-luscious-lasagna/main.rs
+++ src/02-lucian-luscious-lasagna/main.rs
@@ -1,19 +1,20 @@
 fn expected_minutes_in_oven() -> u32 {
-  40
+    40
 }
 
 fn remaining_minutes_in_oven(minutes: u32) -> u32 {
-  expected_minutes_in_oven() - minutes
+    expected_minutes_in_oven() - minutes
 }
 
 fn preparation_time_in_minutes(layers: u32) -> u32 {
-  layers * 2
+    layers * 2
 }
 
 fn elapsed_time_in_minutes(layers: u32, minutes: u32) -> u32 {
-  expected_minutes_in_oven() - remaining_minutes_in_oven(minutes) + preparation_time_in_minutes(layers)
+    expected_minutes_in_oven() - remaining_minutes_in_oven(minutes)
+        + preparation_time_in_minutes(layers)
 }
 
 pub fn main() -> String {
-  elapsed_time_in_minutes(3, 20).to_string()
+    elapsed_time_in_minutes(3, 20).to_string()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
 #![allow(unconditional_recursion)]
 
-#[path = "./01-hello-world/main.rs"] mod hello_world;
-#[path = "./02-lucian-luscious-lasagna/main.rs"] mod lucian_luscious_lasagna;
+#[path = "./01-hello-world/main.rs"]
+mod hello_world;
+#[path = "./02-lucian-luscious-lasagna/main.rs"]
+mod lucian_luscious_lasagna;
 
 pub fn main() {
-  let mut input = String::new();
-  std::io::stdin().read_line(&mut input).unwrap();
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
 
-  print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+    print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
 
-  match input.trim(){
-    "01" => println!("{}", crate::hello_world::main()),
-    "02" => println!("{}", crate::lucian_luscious_lasagna::main()),
-    _ => {},
-  }
+    match input.trim() {
+        "01" => println!("{}", crate::hello_world::main()),
+        "02" => println!("{}", crate::lucian_luscious_lasagna::main()),
+        _ => {}
+    }
 
-  main();
+    main();
 }
