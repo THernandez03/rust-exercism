a7cf6e2ab9ba89748a0abf657978f31189b53183
Tomás Hernández <code@thernandez.me>
2022-09-21 04:12:28 -0300

    refactor: 💡 Remove pub keyword for function on exercise 04

--- src/04-health-statistics/main.rs
+++ src/04-health-statistics/main.rs
@@ -1,19 +1,19 @@
-pub struct User {
+struct User {
     name: String,
     age: u32,
     weight: f32,
 }
 
 impl User {
-    pub fn new(name: String, age: u32, weight: f32) -> Self {
+    fn new(name: String, age: u32, weight: f32) -> Self {
         Self { name, age, weight }
     }
 
-    pub fn set_age(&mut self, new_age: u32) {
+    fn set_age(&mut self, new_age: u32) {
         self.age = new_age;
     }
 
-    pub fn set_weight(&mut self, new_weight: f32) {
+    fn set_weight(&mut self, new_weight: f32) {
         self.weight = new_weight;
     }
 }
